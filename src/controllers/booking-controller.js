0e4723ebb3da2fd92f2ae170fb85a3c84ab5d5b9
Siddhesh202 <siddheshpisal76@gmail.com>
2023-01-21 16:47:47 +0530

    Added some queue related functions

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -16,8 +16,16 @@ class BookingController{
 
     async sendMessageToQueue(req, res){
         const channel = await createChannel();
-        const data = {message: 'SUCCESS'};
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+            service: 'CREATE_TICKET',
+            data: {
+                subject: "This is a notification from queue",
+                content: "Some queue will subscribe this",
+                recepientEmail: "siddheshpisal76@gmail.com",
+                notificationTime: '2023-01-21T17:00:38'
+            }
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
             message: 'Successfully published the event'
         });
